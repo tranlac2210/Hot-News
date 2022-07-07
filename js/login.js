b9d6bae26fbb8d6ff600417550e05a5e6834d1be
tranlac2210 <tranlac3107@gmail.com>
2022-07-07 12:18:06 -0500

    Final Modify

--- js/login.js
+++ js/login.js
@@ -1,9 +1,3 @@
-// const USERS = {
-//   "giaitran": "12101989",
-//   "lactran": "22101989",
-//   "annguyen": "01122000",
-// }
-
 const USERS = [
   {
     user_name: 'giaitran',
@@ -11,22 +5,13 @@ const USERS = [
   },
   {
     user_name: 'lactran',
-    password: '22101989',
+    password: '22101999',
   },
   {
     user_name: 'annguyen',
     password: '01122000',
   },
 ];
-
-// const togglePassword = document.querySelector("#eye-icon");
-// const password = document.querySelector("#password");
-
-// togglePassword.addEventListener("click", function()
-// {
-//   const type = password.getAttribute("type") === "password" ? "text" : "password"; password.getAttribute("type", type)
-//   this.classList.toggle("bi-eye")
-// });
 
 // JQuery
 $(document).on('click', 'button#button-login', function () {
@@ -37,8 +22,7 @@ $(document).on('click', 'button#button-login', function () {
     if (user_name == USERS[i].user_name && password == USERS[i].password) {
       alert('User is authenicated');
       window.location.replace(
-        'C:\\Users\\giait\\Desktop\\Hot News Project\\Hot-News\\index.html?username=' +
-          user_name
+        'index.html?username=' + user_name
       );
       return;
     }
